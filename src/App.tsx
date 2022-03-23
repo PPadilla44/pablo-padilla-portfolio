c2cfc39b388704c9737489d5e376b9f191c71f8f
PPadilla44 <Padilla.pablo1998@gmail.com>
2022-03-23 16:20:37 -0700

    Hero Desktop done

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,22 @@
-import Button from "./components/Button";
+import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 
 
 function App() {
   return (
-    <div className="w-full h-screen bg-60-light">
+    <div className="w-full h-full bg-60-light">
 
+      <Hero />
 
-      <section className="w-full h-full bg-hero bg-no-repeat bg-[length:130%] lg:bg-[length:140%] bg-hero-pos flex items-center justify-center flex-col">
-
-        <div className="max-w-7xl w-full">
-
-          <div className="max-w-2xl w-full text-30-light flex flex-col gap-5 rounded-2xl bg-darkish backdrop-blur-md shadow-around p-5">
-
-            <div className="gap-1 flex flex-col">
-              <h1 className="text-8xl font-bold" >Pablo Padilla</h1>
-              <h3 className="text-4xl font-semibold">Full-Stack Engineer</h3>
-            </div>
-
-            <div className="flex gap-5">
-              <Button className="bg-btn text-xl text-30-light" text="Projects" />
-              <Button className="bg-btn text-xl text-30-light" text="Resume" />
-              <Button className='bg-10-light text-xl text-30-light' text='Contact Me' />
-            </div>
-
-            <p className="text-3xl">
-              I am a committed and enthusiastic full-stack engineer proficient in various programming languages.
-              I am experienced in all levels of web application development, including deployment and maintenance on cloud computing platforms.
-            </p>
-          
-          </div>
-          
-        </div>
-
-      </section>
 
 
       <main className="h-full w-full -mt-20">
-        <Nav transparent={true} />
+        <Nav/>
 
-
-        <main className="h-screen bg-slate-700">
+        <div className="bg-red-300 h-screen">
           dsads
-        </main>
+        </div>
+        <div className="h-screen bg-white"></div>
       </main>
 
     </div>
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,15 +1,41 @@
+import { useEffect, useRef, useState } from 'react'
 import Button from '../Button'
 import Logo from '../Logo'
 
-const Nav = ({ transparent }: { transparent: boolean }) => {
+const Nav = () => {
+
+    const hiddenRef = useRef<HTMLLegendElement>(null);
+    const [color, setColor] = useState(false);
+
+    useEffect(() => {
+
+        window.addEventListener('scroll', scrollHandler);
+
+        return () => window.removeEventListener('scroll', scrollHandler);
+
+    }, []);
+
+    const scrollHandler = () => {
+
+        if (hiddenRef.current) {
+
+            if (window.scrollY >= hiddenRef.current.offsetTop) {
+                setColor(true)
+            } else {
+                setColor(false)
+            }
+        }
+
+    }
+
     return (
-        <nav className={`${!transparent && "bg-30-light"} w-full h-20 sticky top-0 flex left-0 items-center justify-between px-5 z-50 self-end justify-self-end`}>
+        <nav ref={hiddenRef} className={`${color && "bg-30-light"} w-full h-20 sticky top-0 flex left-0 items-center justify-between px-5 z-50 self-end justify-self-end transition-all duration-500`}>
 
             <div className='flex items-center'>
 
                 <Logo className='w-14 h-14 text-6xl font-bold' />
 
-                <div className='flex items-center ml-14 gap-12 text-60-light font-light text-xl'>
+                <div className={` ${color ? "text-black" : "text-60-light"} flex items-center ml-14 gap-12  font-light text-xl`}>
                     <a href='/'>About Me</a>
                     <a href='/'>Skills</a>
                     <a href='/'>Projects</a>
@@ -17,7 +43,7 @@ const Nav = ({ transparent }: { transparent: boolean }) => {
                 </div>
             </div>
 
-            <Button className='border-2 border-60-light text-xl text-30-light hover:bg-10-light hover:border-0' text='Contact Me' />
+            <Button className={` ${color ? "bg-10-light" : "text-60-light border-2 border-60-light"}  text-xl text-30-light hover:bg-10-light hover:border-0`} text='Contact Me' />
 
         </nav>
     )
