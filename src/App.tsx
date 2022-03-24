0eee8cc50ed3538e2e85781703a2cb30c18eee27
PPadilla44 <Padilla.pablo1998@gmail.com>
2022-03-23 21:01:11 -0700

    moved nav to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,71 @@
+import { useRef, useState, useEffect } from "react";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 
 
 function App() {
+
+  const hiddenRef = useRef<HTMLLegendElement>(null);
+  const [color, setColor] = useState(false);
+
+  useEffect(() => {
+
+    window.addEventListener('scroll', scrollHandler);
+
+    return () => window.removeEventListener('scroll', scrollHandler);
+
+  }, []);
+
+  const scrollHandler = () => {
+
+    if (hiddenRef.current) {
+
+      if (window.scrollY >= hiddenRef.current.offsetTop) {
+        setColor(true)
+      } else {
+        setColor(false)
+      }
+    }
+
+  }
+
   return (
     <div className="w-full h-full bg-60-light">
+      <Nav color={color} />
 
       <Hero />
 
+      <div ref={hiddenRef} className="bg-30-light p-20 gap-9 ">
 
+        <section className="flex items-center justify-center mb-20">
+          <div className="max-w-7xl w-full ">
+            <div className="shadow-around w-80 h-80 ">
 
-      <main className="h-full w-full -mt-20">
-        <Nav/>
+              <h1 className="text-8xl font-bold">MORE TEXT HERE</h1>
+            </div>
+          </div>
 
-        <div className="bg-red-300 h-screen">
-          dsads
-        </div>
-        <div className="h-screen bg-white"></div>
-      </main>
+        </section>
+        <section className="flex items-center justify-center mb-20">
+          <div className="max-w-7xl w-full ">
+            <div className="shadow-around w-80 h-80 ">
+
+              <h1 className="text-8xl font-bold">MORE TEXT HERE</h1>
+            </div>
+          </div>
+
+        </section>
+        <section className="flex items-center justify-center">
+          <div className="max-w-7xl w-full ">
+            <div className="shadow-around w-80 h-80 ">
+
+              <h1 className="text-8xl font-bold">MORE TEXT HERE</h1>
+            </div>
+          </div>
+
+        </section>
+
+      </div>
 
     </div>
   );
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,35 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
 import Button from '../Button'
 import Logo from '../Logo'
 
-const Nav = () => {
+const Nav = ({ color }: { color: boolean}) => {
 
-    const hiddenRef = useRef<HTMLLegendElement>(null);
-    const [color, setColor] = useState(false);
 
-    useEffect(() => {
-
-        window.addEventListener('scroll', scrollHandler);
-
-        return () => window.removeEventListener('scroll', scrollHandler);
-
-    }, []);
-
-    const scrollHandler = () => {
-
-        if (hiddenRef.current) {
-
-            if (window.scrollY >= hiddenRef.current.offsetTop) {
-                setColor(true)
-            } else {
-                setColor(false)
-            }
-        }
-
-    }
 
     return (
-        <nav ref={hiddenRef} className={`${color && "bg-30-light"} w-full h-20 sticky top-0 flex left-0 items-center justify-between px-5 z-50 self-end justify-self-end transition-all duration-500`}>
+        <nav className={`${color && "bg-30-light shadow-lg"} w-full h-20 fixed top-0 flex left-0 items-center justify-between px-5 z-50 self-end justify-self-end transition-all duration-500`}>
 
             <div className='flex items-center'>
 
