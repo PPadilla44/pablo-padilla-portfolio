d0cb0558eef4961751faf93d649179da7baf8c3a
PPadilla44 <Padilla.pablo1998@gmail.com>
2022-03-23 21:02:13 -0700

    Added filler text

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,33 @@ function App() {
       <main className="h-full w-full -mt-20">
         <Nav/>
 
-        <div className="bg-red-300 h-screen">
-          dsads
-        </div>
-        <div className="h-screen bg-white"></div>
+        <section className="flex items-center justify-center mb-20">
+          <div className="max-w-7xl w-full ">
+            <div className="shadow-around w-80 h-80 ">
+
+              <h1 className="text-8xl font-bold">MORE TEXT HERE</h1>
+            </div>
+          </div>
+
+        </section>
+        <section className="flex items-center justify-center mb-20">
+          <div className="max-w-7xl w-full ">
+            <div className="shadow-around w-80 h-80 ">
+
+              <h1 className="text-8xl font-bold">MORE TEXT HERE</h1>
+            </div>
+          </div>
+
+        </section>
+        <section className="flex items-center justify-center">
+          <div className="max-w-7xl w-full ">
+            <div className="shadow-around w-80 h-80 ">
+
+              <h1 className="text-8xl font-bold">MORE TEXT HERE</h1>
+            </div>
+          </div>
+
+        </section>
       </main>
 
     </div>
