63f82f12b04d083affb2884a183359a645c4aef0
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2022-04-09 09:57:35 -0700

    added contact form with emailjs

---
+++ src/components/SendButton/SendButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+interface SendButtonProps {
+    isLoading: boolean;
+    isSuccess: boolean;
+}
+
+const SendButton: React.FC<SendButtonProps> = ({ isLoading, isSuccess }) => {
+    return (
+        <button className={`${isLoading && "bg-blue-800 pointer-events-none"}
+        ${isSuccess && "bg-blue-500"}
+        ${!isLoading && !isSuccess && "bg-accent hover:bg-accent/90 active:bg-blue-800"}
+        text-white w-40 rounded-md shadow-md text-lg font-medium mt-3 flex items-center justify-center gap-2`}>
+            {
+                isLoading &&
+                <>
+                    Sending
+                    <div className="loader border-t-teal-500 animate-spin ease-in-out rounded-full border-2 border-t-2 border-gray-200 h-4 w-4" />
+                </>
+            }
+            {
+                isSuccess && "Success!"
+            }
+            {!isLoading && !isSuccess && "Send"}
+        </button>
+    )
+}
+
+export default SendButton
--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -1,13 +1,121 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Section from '../../components/Section'
+import { send } from 'emailjs-com';
+import SendButton from '../../components/SendButton';
+import HeroIcons from '../../components/HeroIcons';
 
 const Contact = () => {
-    return (
-        <Section className='pt-20 flex bg-blue-300 ' >
-            <h1 className='text-center '>Contact</h1>
-            <div className='flex items-center justify-center h-80 bg-blue-300 w-full '>
-                <h2>THE FORM AND CONTACT WILL GO HERE</h2>
 
+    const regex = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$/
+    const [form, setForm] = useState({
+        user_name: "",
+        user_email: "",
+        message: ""
+    });
+    const [errors, setErrors] = useState({
+        nameErr: "",
+        emailErr: "",
+        messageErr: ""
+    })
+    const [success, setSuccess] = useState(false);
+    const [isLoading, setLoading] = useState(false);
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setSuccess(false)
+        setForm(f => ({
+            ...f, [e.target.name]: e.target.value
+        }))
+    }
+
+    const validateForm = () => {
+        const { message, user_email, user_name } = form;
+        let isValid = true;
+        if (message.length < 3) {
+            isValid = false;
+            setErrors(err => ({ ...err, messageErr: "Message must be longer than 2 characters" }))
+        } else {
+            setErrors(err => ({ ...err, messageErr: "" }))
+        }
+
+        if (user_name.length < 2) {
+            isValid = false;
+            setErrors(err => ({ ...err, nameErr: "Name must be longer than 1 character" }))
+        } else {
+            setErrors(err => ({ ...err, nameErr: "" }))
+        }
+
+        if (user_email.length < 3 || !regex.test(user_email)) {
+            isValid = false;
+            setErrors(err => ({ ...err, emailErr: "Invalid Email" }))
+        } else {
+            setErrors(err => ({ ...err, emailErr: "" }))
+        }
+
+        return isValid;
+    }
+
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        let isValid = validateForm();
+        if (!isValid || success) {
+            return
+        }
+        setLoading(true);
+
+        send("service_hfz1snb", 'template_j8pik3h', form, process.env.REACT_APP_EMAIL_USERID)
+            .then((result) => {
+                setSuccess(true);
+                setLoading(false);
+            }, (error) => {
+                setSuccess(false);
+                setLoading(false);
+            });
+
+    }
+
+    return (
+        <Section className='pt-20 pb-6 flex bg-blue-300' >
+            <h1 className='text-center mb-20'>Contact</h1>
+            <div className='flex items-center justify-center w-full flex-col '>
+
+                <form onSubmit={sendEmail} className="flex flex-col gap-1 md:max-w-lg w-full items-center" noValidate>
+                    <div className='w-full'>
+                        <input className={`${errors.nameErr && "border-2 border-red-600"}  w-full rounded-md p-2 text-lg`} type="text" name="user_name" placeholder='Name' value={form.user_name} onChange={handleChange} />
+                        {
+                            errors.nameErr ?
+                                <p className="text-red-600 text-sm text-center">{errors.nameErr}</p>
+                                :
+                                <p>&nbsp;</p>
+                        }
+                    </div>
+                    <div className='w-full'>
+                        <input className={`${errors.emailErr && "border-2 border-red-600"}  w-full rounded-md p-2 text-lg`} type="email" name="user_email" placeholder='Email' value={form.user_email} onChange={handleChange} />
+                        {
+                            errors.emailErr ?
+                                <p className="text-red-600 text-sm text-center">{errors.emailErr}</p>
+                                :
+                                <p>&nbsp;</p>
+                        }
+                    </div>
+                    <div className='w-full'>
+                        <textarea className={`${errors.nameErr && "border-2 border-red-600"}  w-full rounded-md p-2 text-lg resize-none`} rows={4} name="message" value={form.message} placeholder="Message" onChange={handleChange} />
+                        {
+                            errors.messageErr ?
+                                <p className="text-red-600 text-sm text-center" >{errors.messageErr}</p>
+                                :
+                                <p>&nbsp;</p>
+                        }
+                    </div>
+                    <div className='text-center'>
+                        <SendButton isLoading={isLoading} isSuccess={success} />
+                    </div>
+                </form>
+            </div>
+            <div className='flex flex-col gap-2 items-center mt-20'>
+                <HeroIcons />
+                <a className='hover:text-accent' href='mailto:Padilla.Pablo1998@gmail.com'>Padilla.Pablo1998@gmail.com</a>
             </div>
 
         </Section>
