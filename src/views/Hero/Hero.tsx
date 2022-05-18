ea6ec331f7fe463b01e88d7244af5cdd7dcb5fb0
PPadilla44 <Padilla.pablo1998@gmail.com>
2022-05-18 11:39:05 -0700

    text left for hero

--- src/views/Hero/Hero.tsx
+++ src/views/Hero/Hero.tsx
@@ -18,14 +18,7 @@ const Hero = () => {
   return (
     <Element name="about">
       <section className="w-full h-full lg:h-[650px] flex bg-blue-100 justify-center items-center ">
-        <div className="lg:max-w-7xl pb-16 md:pt-16 lg:p-0 h-full lg:h-96 w-full flex items-center lg:justify-between flex-col lg:flex-row lg:px-3 gap-2">
-          <motion.div
-            variants={heroVariants}
-            initial={"offscreen"}
-            whileInView="onscreen"
-            viewport={{ once: true }}
-            className="bg-hero bg-[length:150%] sm:bg-[length:125%]  lg:bg-[length:150%] bg-no-repeat shadow-md bg-hero-pos lg:bg-center min-h-[300px] h-full lg:h-full w-full lg:max-w-xl lg:rounded-md"
-          />
+        <div className="lg:max-w-7xl pb-16 md:pt-16 lg:p-0 h-full lg:h-96 w-full flex items-center lg:justify-between flex-col-reverse lg:flex-row lg:px-3 gap-2">
           <motion.div
             variants={heroVariants}
             initial={"offscreen"}
@@ -47,6 +40,13 @@ const Hero = () => {
 
             <HeroIcons />
           </motion.div>
+          <motion.div
+            variants={heroVariants}
+            initial={"offscreen"}
+            whileInView="onscreen"
+            viewport={{ once: true }}
+            className="bg-hero bg-[length:150%] sm:bg-[length:125%]  lg:bg-[length:150%] bg-no-repeat shadow-md bg-hero-pos lg:bg-center min-h-[300px] h-full lg:h-full w-full lg:max-w-xl lg:rounded-md"
+          />
         </div>
       </section>
     </Element>
--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -6,7 +6,7 @@ import { ProjectData } from '../../data/ProjectData'
 const Projects = () => {
     return (
         <Section name='projects' className='flex py-20 bg-white'>
-            <h1 className='text-center md:text-right text-black mb-20'>Projects</h1>
+            <h1 className='text-center md:text-left text-black mb-20'>Projects</h1>
 
             <div className='flex gap-28 flex-col'>
                 {
