9e403909ce929106c642b3e12d658092774667b4
PPadilla44 <padilla.pablo1998@gmail.com>
2023-03-23 11:04:12 -0700

    Added ph experience

--- src/data/ExperienceData.ts
+++ src/data/ExperienceData.ts
@@ -1,24 +1,31 @@
 import { ExperienceBlockProps } from "../components/ExperienceBlock/ExperienceBlock";
 
-
 export const ExperienceData: ExperienceBlockProps[] = [
-    {
-        title: "Teaching Assistant",
-        subTitle: "Coding Dojo",
-        description: `Support Instructors by assisting in teaching through small lectures, live demos, and frequent check-ins with students to identify what students 
+  {
+    title: "Software Engineer",
+    subTitle: "Players Health",
+    description: `Developing and maintaining software applications by collaborating with other team members to implement new features and improvements. Expertise in full-stack development and experience with financial applications.`,
+    location: "Remote",
+    duration: "September 2022 - Current",
+    image: "/images/ph_logo.png",
+  },
+  {
+    title: "Teaching Assistant",
+    subTitle: "Coding Dojo",
+    description: `Support Instructors by assisting in teaching through small lectures, live demos, and frequent check-ins with students to identify what students 
             are working on and how to support their learning.`,
-        location: "Remote",
-        duration: "September 2021 - Current",
-        image: "/images/dojo.png",
-    },
-    {
-        title: "Full-Stack Engineer",
-        subTitle: "Fast Way Hydroponics",
-        description: `Collaborated with a co-founder to deliver an internal web/mobile app. 
+    location: "Remote",
+    duration: "September 2021 - September 2022",
+    image: "/images/dojo.png",
+  },
+  {
+    title: "Full-Stack Engineer",
+    subTitle: "Fast Way Hydroponics",
+    description: `Collaborated with a co-founder to deliver an internal web/mobile app. 
         And worked with a small team of software engineers and facilitated workloads while tackling full-stack duties.`,
-        location: "Remote",
-        duration: "Feburary 2021 - March 2022",
-        image: "/images/fastway.png",
-        textAsImage: ["Fast Way", "Hydroponics"]
-    },
-]
+    location: "Remote",
+    duration: "Feburary 2021 - March 2022",
+    image: "/images/fastway.png",
+    textAsImage: ["Fast Way", "Hydroponics"],
+  },
+];
